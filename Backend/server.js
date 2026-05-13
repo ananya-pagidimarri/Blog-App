5a207f10870b5fbe65415440e20dd5465b8a64a6
ananya-pagidimarri <ananya.pagidimarri@gmail.com>
2026-05-13 18:09:52 +0530

    fixed cors credentials

--- Backend/server.js
+++ Backend/server.js
@@ -21,10 +21,7 @@ app.set("trust proxy", 1);
 // CORS
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://blog-app-ahtk.vercel.app",
-    ],
+    origin: "https://blog-app-ahtk.vercel.app",
     credentials: true,
   })
 );
